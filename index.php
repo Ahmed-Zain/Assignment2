960471b7f17a79a4472b6e3ff035c445c89704df
Ahmed-Zain <45338500+Ahmed-Zain@users.noreply.github.com>
2018-11-25 23:58:33 +0500

    Add files via upload
    Files added

---
+++ index.php
@@ -0,0 +1,304 @@
+<!DOCTYPE html>
+
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <script src="js/main.js"></script>
+    <title>Home</title>
+
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+    <link rel="stylesheet" href="css/styling.css" type="text/css">
+
+</head>
+
+<body background="img/offix.jpg">
+
+
+<header>
+
+    <div class="Top_Row">
+
+        <div id="strip">
+
+
+            <div class="Top-Text">
+
+                <ul>
+                   <li><a>Ucp Johar Town, Lahore</a></li>
+                    <li><a>|</a></li>
+                    <li><a>Call us on:</a></li>
+                    <li> <a style="color: pink"> 090078601 </a> </li>
+                </ul>
+
+        </div>
+
+            <div class="RightTop_Text">
+                <ul>
+                    <li><a href="#"><u>Faq</u></a></li>
+                    <li><a>|</a></li>
+                    <li><a>Ahmed Zain</a></li>
+
+                </ul>
+            </div>
+
+
+        </div>
+
+    </div>
+</header>
+<div class="dropdown">
+    <button class="dropbtn">Element</button>
+    <div class="dropdown-content">
+        <a href="#">What I Can Do</a>
+        <a href="#">A Little Bit About Me</a>
+        <a href="#">Say Hello</a>
+    </div>
+</div>
+
+
+<div class="dropdown">
+    <button class="dropbtn">Shop</button>
+    <div class="dropdown-content">
+        <a href="#">What I Can Do</a>
+        <a href="#">A Little Bit About Me</a>
+        <a href="#">Say Hello</a>
+    </div>
+</div>
+
+
+<div class="dropdown">
+    <button class="dropbtn">Blog</button>
+    <div class="dropdown-content">
+        <a href="#">What I Can Do</a>
+        <a href="#">A Little Bit About Me</a>
+        <a href="#">Say Hello</a>
+    </div>
+</div>
+
+
+
+<div class="dropdown">
+    <button class="dropbtn">Portfolio</button>
+    <div class="dropdown-content">
+        <a href="#">What I Can Do</a>
+        <a href="#">A Little Bit About Me</a>
+        <a href="#">Say Hello</a>
+    </div>
+</div>
+
+<div class="dropdown">
+    <button class="dropbtn">Pages</button>
+    <div class="dropdown-content">
+        <a href="#">What I Can Do</a>
+        <a href="#">A Little Bit About Me</a>
+        <a href="#">Say Hello</a>
+    </div>
+</div>
+
+<div class="dropdown">
+    <button class="dropbtn">Home</button>
+    <div class="dropdown-content">
+        <a href="#">What I Can Do</a>
+        <a href="#">A Little Bit About Me</a>
+        <a href="#">Say Hello</a>
+    </div>
+</div>
+
+<div class="Logo">
+
+    <img src="img/ucp-logo.png"width="288" height="98"/>
+
+</div>
+
+
+<div class="MidText">
+    <h1>Hi,UCP</h1>   <div> </div>
+    <h6>Class Activity</h6>
+</div>
+
+
+<div class="Second-Half">
+
+    <table border="0" align="center">
+
+        <tr> <td> <h1> Blank </h1> </td> </tr>
+
+        <tr class="Main-Heading-SH">
+    <td align="center"> What I Can Do</td>
+        </tr>
+    </table>
+
+    <div class="Under-Headings-SH">
+
+    <h4>  Fully Responsive  </h4>
+        <h4>  Powerful Portfolios  </h4>
+            <h4>  Amazing Shortcodes  </h4>
+
+
+        <table>
+
+            <tr>
+
+        <td>  When a crisis hits, your people will rely on you for quick, effective communication
+            - and you can rely on us. As the original emergency notification system, AlertFind has
+            been in business for over 20 years. We’ve helped our customers weather every storm and
+            we’ve got you covered.   </td>
+
+        <td>  When a crisis hits, your people will rely on you for quick, effective communication -
+            and you can rely on us. As the original emergency notification system, AlertFind has been
+            in business for over 20 years. We’ve helped our customers weather every storm and we’ve got you covered.   </td>
+
+        <td>  When a crisis hits, your people will rely on you for quick, effective communication
+            - and you can rely on us. As the original emergency notification system, AlertFind has
+            been in business for over 20 years. We’ve helped our customers weather every storm and
+            we’ve got you covered.   </td>
+
+            </tr>
+        </table>
+
+    </div>
+
+
+    <div class="Under-Description-SH">
+
+        <h3> A Little Bit About Me </h3>
+
+    </div>
+
+
+    <div class="Profile-Pic">
+
+
+        <table border="1" class="Profile-Stuff-1">
+
+            <tr>
+
+                <td>
+
+                    <img src="img/Bajwa.jpg" width="250" height="300">
+
+                </td>
+            </tr>
+
+        </table>
+
+        <table class="Profile-Stuff-2">
+
+            <tr>
+
+        <td><b>As it so contrasted oh estimating instrument. Size like body some one had. Merits ye if mr narrow points.
+            Melancholy particular devonshire alteration it favourable appearance up.</b></td>
+                </tr>
+
+            <tr class="Right-Profile-Text">
+
+                <td>
+                    As it so contrasted oh estimating instrument. Size like body some one had. Merits ye if mr narrow points.
+                    Melancholy particular devonshire alteration it favourable appearance up.
+                    As it so contrasted oh estimating instrument. Size like body some one had. Merits ye if mr narrow points.
+                </td>
+
+            <tr class="Right-Profile-Text">
+            <td>
+                    Melancholy particular devonshire alteration it favourable appearance up.
+                    As it so contrasted oh estimating instrument. Size like body some one had. Merits ye if mr narrow points.
+                    Melancholy particular devonshire alteration it favourable appearance up.
+
+                </td>
+
+
+            </tr>
+
+        </table>
+
+    </div>
+
+    <div class="comment-form">
+
+        <form name="RegForm" action="" onsubmit="return formvalidatefunc()" method="post">
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label">Name</label>
+                <div class="col-sm-10">
+                    <input type="text" size="65" name="Name" class="form-control">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label">E-mail</label>
+                <div class="col-sm-10">
+                    <input type="text" size="65" name="EMail" class="form-control">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label">Password</label>
+                <div class="col-sm-10">
+                    <input type="password" size="65" name="Password" class="form-control">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label">Address</label>
+                <div class="col-sm-10">
+                    <input type="text" size="65" name="Address" class="form-control">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label">Telephone</label>
+                <div class="col-sm-10">
+                    <input type="text" size="65" name="Telephone" class="form-control">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label">Course</label>
+                <div class="col-sm-10">
+                    <select class="form-control" name="Subject">
+                        <option value="">None</option>
+                        <option value="bscs">BSCS</option>
+                        <option value="bba">BBA</option>
+                        <option value="bca">BCA</option>
+                    </select>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label">Comments</label>
+                <div class="col-sm-10">
+                    <textarea cols="55" name="Comment" class="form-control"></textarea>
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-2">
+                    <button type="submit" name="Submit" class="btn btn-primary">Send</button>
+                </div>
+                <div class="col-sm-2">
+                    <button type="reset" name="Reset" class="btn btn-secondary">Reset</button>
+                </div>
+            </div>
+
+            <p></p>
+        </form>
+
+
+
+    </div>
+
+</div>
+
+
+</body>
+
+<footer>
+
+
+
+
+</footer>
+
+
+
+</html>
